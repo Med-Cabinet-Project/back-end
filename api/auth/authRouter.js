26203a1c84f13a953876c8b42f0a60a9ace70f53
Jose <pjose14.jp38@yahoo.com>
2020-06-17 21:37:09 -0500

    authRouter is settup. working on register and login

---
+++ api/auth/authRouter.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+const express = require('express');
+
+
+const router = express.Router();
+
+// Register
+router.post('/register', (req, res) => {
+
+})
+
+// Login
+router.post('/login', (req, res) => {
+
+})
+
+module.exports = router;
