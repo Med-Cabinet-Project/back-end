6a96735d4d4be29658148997ff6a1e2c3ae92ed6
Jose <pjose14.jp38@yahoo.com>
2020-06-24 21:54:42 -0500

    quick add on

---
+++ data/seeds/003-strains.js
@@ -0,0 +1,29 @@
+
+exports.seed = knex => {
+  return knex('strains')
+    .insert([
+      { strain: 'Ak-47' },
+      { strain: 'Afghani' },
+      { strain: 'Alohaberry' },
+      { strain: 'Bakerstreet' },
+      { strain: 'Bio Jesus' },
+      { strain: 'Birthday Cake Kush' },
+      { strain: 'Blueberry Dream' },
+      { strain: 'Brain Berry Cough' },
+      { strain: 'Bubbleberry' },
+      { strain: 'Cackleberry' },
+      { strain: 'Candy Cane' },
+      { strain: 'Double Dutch' },
+      { strain: 'Doctor Funk' },
+      { strain: 'Dragons Breath' },
+      { strain: 'Durban Poison' },
+      { strain: 'Ebola 007' },
+      { strain: 'Electric Kool-Aid' },
+      { strain: 'Flaming Cookies' },
+      { strain: 'Golden Nugget' },
+      { strain: 'Head Cheese' },
+      { strain: 'Lotion Potion ' },
+      { strain: 'Snack Attack' },
+      { strain: 'Rick Roll' }
+    ])
+}
