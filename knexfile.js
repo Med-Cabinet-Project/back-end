3564c09db0f0ba1a96a9438dcc3e6c787cb885c1
Jose <pjose14.jp38@yahoo.com>
2020-06-24 21:37:45 -0500

    pushing knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -44,8 +44,11 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   }
 
 };
